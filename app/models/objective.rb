93255b11cc69966f6e5db114df7c97555fd4ea8d
Sidney Martin <sidney.martin@ik.me>
2025-12-04 15:28:23 +0100

    some details on the new obj (require inputs, dropdown for time per day, ...)

--- app/models/objective.rb
+++ app/models/objective.rb
@@ -1,4 +1,8 @@
 class Objective < ApplicationRecord
   belongs_to :user
   has_many :todos, dependent: :destroy
+
+  validates :goal, presence: true
+  validates :time_global, presence: true
+  validates :time_due, presence: true
 end
