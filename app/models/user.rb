bec51c3bf42f44a85199ffe34e5f134026e2ab0c
Sebastien Richard <sebastienrlr@gmail.com>
2025-12-08 12:18:17 +0100

    Gamifier

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,4 +4,40 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
   has_many :objectives, dependent: :destroy
+
+
+  def completed_objectives_count
+    objectives.where(completed: true).count
+  end
+
+  def badge_level
+    case completed_objectives_count
+    when 0
+      :none
+    when 1..2
+      :bronze
+    when 3..4
+      :silver
+    else
+      :gold
+    end
+  end
+
+  def badge_image
+    {
+      none:   "debutant.png",
+      bronze: "bronze.png",
+      silver: "argent.png",
+      gold:   "or.png"
+    }[badge_level]
+  end
+
+  def badge_label
+    {
+      none:   "Débutant",
+      bronze: "Bronze",
+      silver: "Argent",
+      gold:   "Or"
+    }[badge_level]
+  end
 end
